7fc2db4f9195a7383d01c9577c2f6994ba01f304
kiran@simpledatalabs.com <kiran@simpledatalabs.com>
2023-03-30 13:48:44 +0000

    🔁 Modified     - Macro        - cents_to_dollars        - just_concat

--- new_project/path/one/macros/cents_to_dollar.sql
+++ new_project/path/one/macros/cents_to_dollar.sql
@@ -1,8 +1,7 @@
-{% macro cents_to_dollars(column_name, decimal_places=2
-) %}
-round( 1.0 * {{ column_name }} / 100, {{decimal_places}} )
-{% endmacro %}
-
 {% macro just_concat() %}
 select * from dbt_alice.test limit 1
+{% endmacro %}
+
+{% macro cents_to_dollars(column_name, decimal_places=2) %}
+round( 1.0 * {{ column_name }} / 100, {{decimal_places}} )
 {% endmacro %}
