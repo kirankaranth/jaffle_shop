8881168da269c2a5ccc3bddc45ce1f2e726f5d76
kiran@simpledatalabs.com <kiran@simpledatalabs.com>
2023-03-30 13:27:53 +0000

    Cloning prophecy project main branch

---
+++ macros/cents_to_dollar.sql
@@ -0,0 +1,8 @@
+{% macro cents_to_dollars(column_name, decimal_places=2
+) %}
+round( 1.0 * {{ column_name }} / 100, {{decimal_places}} )
+{% endmacro %}
+
+{% macro just_concat() %}
+select * from dbt_alice.test limit 1
+{% endmacro %}
