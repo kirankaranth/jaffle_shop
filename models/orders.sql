8994b31da3fcbb85412443665038c6aefdf9219b
kiran@simpledatalabs.com <kiran@simpledatalabs.com>
2023-04-19 05:13:22 +0000

    🔁 Modified     - Model        - orders

--- models/orders.sql
+++ models/orders.sql
@@ -1,74 +1,70 @@
-
-{{ config({
-        "materialized": "incremental",
-        "tags": ["orders_snapshots"],
-        "alias": "orders"
-    })
+{{
+  config({    
+    "materialized": "incremental",
+    "tags": ["orders_snapshots"],
+    "alias": "orders"
+  })
 }}
 
 {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
-
-{% set payments_dict = {
-    'credit':'card10',
-    'bank':'transfer11'
-} %}
+{% set payments_dict = { 'credit' : 'card10', 'bank' : 'transfer11' } %}
 
 
-with orders as (
+WITH payments AS (
 
-    select * from {{ ref('stg_orders') }}
+  SELECT * 
+  
+  FROM {{ ref('stg_payments')}}
 
 ),
 
-payments as (
+order_payments AS (
 
-    select *
-
-    from {{ ref('stg_payments') }}
-
-),
-
-order_payments as (
-
-    select
-        order_id,
-        -- {{my_abc}},
-        -- {{payment_methods}},
-        -- {{ var('my_payment_methods') }},
-        {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
-        {% endfor -%}
-
-        sum(amount) as total_amount
-
-    from payments
-
-    group by order_id
+  SELECT 
+    order_id,
+    {% for payment_method in payment_methods %}
+      sum(CASE
+        WHEN payment_method = '{{ payment_method }}'
+          THEN amount
+        ELSE 0
+      END) AS {{payment_method}}_amount,
+    {% endfor %}
+    
+    sum(amount) AS total_amount
+  
+  FROM payments
+  
+  GROUP BY order_id
 
 ),
 
-final as (
+orders AS (
 
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        orders.status,
+  SELECT * 
+  
+  FROM {{ ref('stg_orders')}}
 
-        {% for payment_method in payment_methods -%}
+),
 
-        order_payments.{{ payment_method }}_amount,
+final AS (
 
-        {% endfor -%}
-
-        order_payments.total_amount as amount
-
-    from orders
-
-
-    left join order_payments
-        on orders.order_id = order_payments.order_id
+  SELECT 
+    orders.order_id,
+    orders.customer_id,
+    orders.order_date,
+    orders.status,
+    {% for payment_method in payment_methods %}
+      order_payments.{{payment_method}}_amount,
+    {% endfor %}
+    
+    order_payments.total_amount AS amount
+  
+  FROM orders
+  LEFT JOIN order_payments
+     ON orders.order_id = order_payments.order_id
 
 )
 
-select * from final
+SELECT *
+
+FROM final
